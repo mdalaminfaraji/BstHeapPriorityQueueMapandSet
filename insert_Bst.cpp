d2ec356a07adeed425175bf0ea519f148c1164ff
mdalaminfaraji <alaminice1617@gmail.com>
2023-07-21 12:26:50 +0600

    inser bst

---
+++ insert_Bst.cpp
@@ -0,0 +1,110 @@
+#include<bits\stdc++.h>
+using namespace std;
+ class Node
+{
+    public:
+    int val;
+    Node* left;
+    Node* right;
+    Node(int val){
+        this->val=val;
+        this->left=NULL;
+        this->right=NULL;
+    }
+};
+Node* input_tree(){
+    int val;
+    cin>>val;
+    Node* root;
+  
+    if(val==-1) root=NULL;
+    else root=new Node(val);
+   
+    queue<Node*> q;
+    if(root) q.push(root);
+    while (!q.empty())
+    {
+        Node* p=q.front();
+        q.pop();
+        // 2. jabotiyo ja kaj ache
+        int l, r;
+        cin>>l>>r;
+        Node *myLeft;
+        Node *myRight;
+        if(l==-1) myLeft=NULL;
+        else
+        myLeft=new Node(l);
+       
+
+        if(r==-1) myRight=NULL;
+        else
+        myRight=new Node(r);
+        p->left=myLeft;
+        p->right=myRight;
+        //3.children gulo ke push koro
+        if(p->left){
+            q.push(p->left);
+        }
+        if(p->right)  q.push(p->right);
+    }
+        return root;
+    
+}
+void level_order(Node* root){
+    queue<Node*>q;
+ 
+  if(root == NULL){
+      return;
+  }
+     q.push(root);
+  
+  
+    while(!q.empty()){
+        //ber kore ana
+
+        Node* f=q.front();
+      
+        q.pop();
+        //ja kaj ta kor
+        cout<<f->val<<" ";
+        //childron gulo add kor
+        if(f->left)q.push(f->left);
+        if(f->right)q.push(f->right);
+    }
+}
+bool search(Node *root, int x){
+    if(root==NULL)return false;
+    if(root->val==x)return true;
+    if(x<root->val){
+        return search(root->left, x);
+    }
+    else{
+        return search(root->right, x);
+    }
+}
+void insert(Node* &root, int x){
+  if(root==NULL){
+    root=new Node(x);
+  }
+  if(x<root->val){
+    if(root->left==NULL){
+        root->left=new Node(x);
+    }else{
+        insert(root->left,x);
+    }
+  }else{
+     if(root->right==NULL){
+        root->right=new Node(x);
+    }else{
+        insert(root->right,x);
+    }
+  }
+}
+int main()
+{
+    Node* root=input_tree();
+    int x; cin>>x;
+    insert(root, x);
+    level_order(root);
+    return 0;
+}
